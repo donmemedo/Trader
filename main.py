ca3c473f15234fcecd21262115da8279ea0d8ef7
donmemedo <49355677+donmemedo@users.noreply.github.com>
2022-05-30 17:30:02 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from coinex.coinex import CoinEx
+import numpy as np
 # from somewhere_else import access_id, secret
 
 coinex = CoinEx('208C0621123240C48DBA805C7C172412', '83FC10D422EF11CB57C5EAFF9F2F2B145DB79144281B8502')
 
-# public API
-# coinex.market_list()
-# a=coinex.market_ticker('BCHBTC')
-b=coinex.market_ticker_all()
-# coinex.market_depth('BCHBTC')
-# coinex.market_deals('BCHBTC')
+b = coinex.market_ticker_all()
+coins = b['ticker']
+name = list(coins.keys())
+liste = np.zeros((10, len(name)))
+for i in range(len(name)):
+    liste[1, i] = float(coins[name[i]]['low'])  # low
+    liste[2, i] = float(coins[name[i]]['high'])  # high
+    liste[0, i] = float(coins[name[i]]['last'])  # last
+    liste[5, i] = (liste[2, i]-liste[1, i])/liste[1, i]  # Profit
+    liste[6, i] = (1-liste[5, i]/2)*liste[0, i]  # Temp Buy
+    liste[7, i] = (1+liste[5, i]/2)*liste[0, i]  # Temp Sell
+    liste[3, i] = max(liste[6, i], liste[1, i])  # Buy
+    liste[4, i] = min(liste[7, i], liste[2, i])  # Sell
+    liste[8, i] = 100*(liste[4, i]-liste[3, i])/liste[3, i]  # My Profit
+    liste[9, i] = liste[8, i]/liste[5, i]  # Ratio
 
-# private API
-# coinex.balance_info()
-# coinex.balance_coin_withdraw_list()
-# coinex.balance_coin_withdraw('BCH', 'qzjtuuc9nafvfhmwt47z84awltp9gmx7wyma3kvy9v', 0.001)
-# coinex.balance_coin_withdraw_list(coin_withdraw_id=2465345342)
-# coinex.balance_coin_withdraw_cancel(coin_withdraw_id=2465345342)
-# coinex.order_limit('CETBCH', 'sell', 100.0, 10.0)
-# coinex.order_market('CETBCH', 'buy', 1.0)
-# coinex.order_ioc('CETBCH', 'sell', 100.0, 10.0)
-# coinex.order_pending('CETBCH')
-# coinex.order_finished('CETBCH')
-# coinex.order_status('CETBCH', 2465345342)
-# coinex.order_deals(2465345342)
-# coinex.order_user_deals('CETBCH')
-# coinex.order_pending_cancel('CETBCH', 2465345342)
-# coinex.order_mining_difficulty()
